b9f4426e9905ce31f12823fea587cf3a62a910e8
congben <133671859+congben@users.noreply.github.com>
2024-08-07 08:52:13 +0700

    Update page.tsx

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src="https://scontent.fhan3-3.fna.fbcdn.net/v/t39.30808-6/423716525_1428551691403099_208379151873273952_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=XktXprNo-10Q7kNvgFHoSnw&_nc_ht=scontent.fhan3-3.fna&oh=00_AYC0eN_PwdNXOoYiUjAhh_0tfPCnjnHDkClca-Kii3j18Q&oe=66B8B7A7"
           alt="Next.js Logo"
           width={180}
           height={37}
